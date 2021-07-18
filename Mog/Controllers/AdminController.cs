11c485d3bb5a991ab264f4d4adfc4c88ac811249
chetan marisetty <41148829+chetanchet@users.noreply.github.com>
2021-07-18 20:07:09 +0530

    4 done

---
+++ Mog/Controllers/AdminController.cs
@@ -0,0 +1,60 @@
+﻿using Mog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mog.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            using (var _context = new ApplicationDbContext())
+            {
+                var userList = _context.BranchAdmins.Where(x=>!x.IsApproved).ToList();
+                return View(userList);
+            }
+            return View();
+        }
+
+        public ActionResult Approve(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var admin = db.BranchAdmins.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            admin.IsApproved = true;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var admin = db.BranchAdmins.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            db.BranchAdmins.Remove(admin);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
